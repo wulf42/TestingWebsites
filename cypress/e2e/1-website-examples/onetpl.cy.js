4e23ec19be116d0e51d4cef5585ee7ca7b04c8e4
Wojciech Pakoca <wojtekpako2@gmail.com>
2023-10-27 16:04:08 +0200

    navbar test improved

--- cypress/e2e/1-website-examples/onetpl.cy.js
+++ cypress/e2e/1-website-examples/onetpl.cy.js
@@ -2,10 +2,6 @@ describe('Testing onet.pl',() => {
 
     beforeEach(() => {
         cy.visit('https://www.onet.pl');  
-        cy.setCookie(
-            "cookies_consent",
-            JSON.stringify({ cookies_analytics: "granted", cookies_marketing: "granted" })
-            )      
     })
 
     it('contains Onet in title', () => {
@@ -22,29 +18,26 @@ describe('Testing onet.pl',() => {
         cy.get('[class^="Menu_navWrapper"]').should('be.visible');
         cy.get('[class^="Menu_navMenuListItem"]').should('be.visible');
     })
-    it('navbar contains correct names',()=>{
-       cy.get('[class^="Menu_navMenuListItem"]').eq(0).should('contain','Wiadomości');
-       cy.get('[class^="Menu_navMenuListItem"]').eq(1).should('contain','Sport');
-       cy.get('[class^="Menu_navMenuListItem"]').eq(2).should('contain','Premium');
-       cy.get('[class^="Menu_navMenuListItem"]').eq(3).should('contain','Biznes');
-       cy.get('[class^="Menu_navMenuListItem"]').eq(4).should('contain','Regionalne');
-       cy.get('[class^="Menu_navMenuListItem"]').eq(5).should('contain','Pogoda');
-       cy.get('[class^="Menu_navMenuListItem"]').eq(6).should('contain','Wideo i audio');
-       cy.get('[class^="Menu_navMenuListItem"]').eq(7).should('contain','Motoryzacja');
-       cy.get('[class^="Menu_navMenuListItem"]').eq(8).should('contain','Nieruchomości');
+    
+    it('navbar contains correct names and links',()=>{
+        const manuPositions = [
+            {name: 'Wiadomości', link: 'https://wiadomosci.onet.pl/'},
+            {name: 'Sport', link: 'https://przegladsportowy.onet.pl/'},
+            {name: 'Premium', link: 'https://www.onet.pl/premium'},
+            {name: 'Biznes', link: 'https://businessinsider.com.pl/'},
+            {name: 'Regionalne', link: '#'},
+            {name: 'Pogoda', link: 'https://pogoda.onet.pl/'},
+            {name: 'Wideo i audio', link: 'https://video.onet.pl/'},
+            {name: 'Motoryzacja', link: 'https://www.auto-swiat.pl/'},
+            {name: 'Nieruchomości', link: 'https://www.morizon.pl/?utm_source=Onet-RASP&utm_medium=link&utm_campaign=onet-nav'}
+        ]
+
+        cy.get('[class^="Menu_navMenuListItem"]').each(($element, index) => {
+            cy.wrap($element).should('contain', manuPositions[index].name);
+          });
+        cy.get('[class^="Menu_navMenuLink"]').each(($link, index) => {
+            cy.wrap($link).should('have.attr', 'href', manuPositions[index].link);
+          });
     })
-    it('navbar contains correct link',()=>{
-        cy.get('[class^="Menu_navMenuLink"]').eq(0).should('have.attr','href','https://wiadomosci.onet.pl/');
-        cy.get('[class^="Menu_navMenuLink"]').eq(1).should('have.attr','href','https://przegladsportowy.onet.pl/');
-        cy.get('[class^="Menu_navMenuLink"]').eq(2).should('have.attr','href','https://www.onet.pl/premium');
-        cy.get('[class^="Menu_navMenuLink"]').eq(3).should('have.attr','href','https://businessinsider.com.pl/');
-        cy.get('[class^="Menu_navMenuLink"]').eq(5).should('have.attr','href','https://pogoda.onet.pl/');
-        cy.get('[class^="Menu_navMenuLink"]').eq(6).should('have.attr','href','https://video.onet.pl/');
-        cy.get('[class^="Menu_navMenuLink"]').eq(7).should('have.attr','href','https://www.auto-swiat.pl/');
-        cy.get('[class^="Menu_navMenuLink"]').eq(8).should('have.attr','href','https://www.morizon.pl/?utm_source=Onet-RASP&utm_medium=link&utm_campaign=onet-nav');
-
-     })
-
-
-
+      
 })
